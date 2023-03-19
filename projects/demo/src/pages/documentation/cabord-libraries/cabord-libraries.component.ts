db3aa7cc0138d111af3e9877c8e074b982d995c5
Aleksandr Shatilov <ashatilovdev@gmail.com>
2023-03-19 23:43:07 +0300

    feat: fix

--- projects/demo/src/pages/documentation/cabord-libraries/cabord-libraries.component.ts
+++ projects/demo/src/pages/documentation/cabord-libraries/cabord-libraries.component.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {DemoPath} from '@demo/routes';
 
 @Component({
     selector: 'app-cabord-libraries',
     templateUrl: './cabord-libraries.component.html',
     styleUrls: ['./cabord-libraries.component.less'],
 })
-export class CabordLibrariesComponent {}
+export class CabordLibrariesComponent {
+    readonly coreConceptsOverviewDocPage = `/${DemoPath.CoreConceptsOverview}`;
+    readonly angularDocPage = `/${DemoPath.Angular}`;
+}
