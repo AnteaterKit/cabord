46cadcad7c3c7b0f8c23a730ef082f8b10b82244
Aleksandr Shatilov <ashatilovdev@gmail.com>
2023-03-20 23:08:15 +0300

    feat: test stage

--- projects/kit/src/lib/board/board.spec.ts
+++ projects/kit/src/lib/board/board.spec.ts
@@ -1,14 +1,12 @@
 import {Caboard} from './board';
-import 'jest-canvas-mock';
-
-beforeEach(() => {
-    jest.resetAllMocks();
-    document.body.innerHTML = '<div><canvas id="caboard" /></div>';
-});
 
 describe('Caboard', () => {
-    const element = document.getElementById('caboard') as HTMLDivElement;
-    it('should work', () => {
-        expect(new Caboard(element, null)).toEqual('Caboard');
+    document.body.innerHTML = '<div><div id="caboard" ><div></div>';
+    const element = document.getElementById('caboard') as any;
+
+    it('stage should work', () => {
+        const caboard = new Caboard(element, {width: 200, height: 200});
+        caboard.render();
+        expect(!!caboard.stage).toEqual(true);
     });
 });
